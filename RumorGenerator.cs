1e434a7138414265a7b456cb6b43431e6db78f8a
fonducando <calandro3@hotmail.com>
2022-05-10 01:46:11 -0400

    Windows Form

--- RumorGenerator.cs
+++ RumorGenerator.cs
@@ -6,16 +6,16 @@ namespace Rumor_Mill_Generator
     public class RumorGenerator
     { 
     
-        public static Random name1 = new Random(), verb1 = new Random(), name2 = new Random(), verb2 = new Random(), noun1 = new Random(), name3 = new Random();
+        public static System.Random name1 = new System.Random(), verb1 = new System.Random(), name2 = new System.Random(), verb2 = new System.Random(), noun1 = new System.Random(), name3 = new System.Random();
 
         public static string GenerateRandomPhrase()
         {
-        int name1num = name1.Next(1, (File.ReadAllLines(Directory.GetCurrentDirectory() + "/names.txt").Count()));
-        int name2num = name2.Next(1, (File.ReadAllLines(Directory.GetCurrentDirectory() + "/names.txt").Count()));
-        int name3num = name2.Next(1, (File.ReadAllLines(Directory.GetCurrentDirectory() + "/names.txt").Count()));
-        int verb1num = verb1.Next(1, (File.ReadAllLines(Directory.GetCurrentDirectory() + "/verbs1.txt").Count()));     
-        int verb2num = verb2.Next(1, (File.ReadAllLines(Directory.GetCurrentDirectory() + "/verbs2.txt").Count()));
-        int noun1num = noun1.Next(1, (File.ReadAllLines(Directory.GetCurrentDirectory() + "/nouns.txt").Count()));
+        int name1num = name1.Next(1, (File.ReadAllLines(Directory.GetCurrentDirectory() + "/names.txt").Length));
+        int name2num = name2.Next(1, (File.ReadAllLines(Directory.GetCurrentDirectory() + "/names.txt").Length));
+        int name3num = name2.Next(1, (File.ReadAllLines(Directory.GetCurrentDirectory() + "/names.txt").Length));
+        int verb1num = verb1.Next(1, (File.ReadAllLines(Directory.GetCurrentDirectory() + "/verbs1.txt").Length));     
+        int verb2num = verb2.Next(1, (File.ReadAllLines(Directory.GetCurrentDirectory() + "/verbs2.txt").Length));
+        int noun1num = noun1.Next(1, (File.ReadAllLines(Directory.GetCurrentDirectory() + "/nouns.txt").Length));
 
         string stringtowrite = ((File.ReadAllLines("names.txt")[name1num]) + " said " + (File.ReadAllLines("names.txt")[name2num]) + " " + (File.ReadAllLines(Directory.GetCurrentDirectory() + "/verbs1.txt")[verb1num]) + " " + ((File.ReadAllLines("names.txt")[name3num])) + " " + (File.ReadAllLines(Directory.GetCurrentDirectory() + "/verbs2.txt")[verb2num]) + " " + (File.ReadAllLines(Directory.GetCurrentDirectory() + "/nouns.txt")[noun1num]));
        
